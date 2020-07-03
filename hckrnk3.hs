5ecb80ab2222d7bb61861859dc9e534af2870212
shadowfacsimile <shadow.facsimile@gmail.com>
2020-07-03 14:20:50 +0530

    Staircase problem

--- hckrnk3.hs
+++ hckrnk3.hs
@@ -2,3 +2,5 @@ comp :: String -> String
 comp s = (show . length . filter (\x -> x > 0)) xs ++ " " ++ (show . length . filter (\x -> x < 0)) xs
              where xs = [(x - y) | (x, y) <- xz]
                    xz = zip (map read $ take 3 $ words s :: [Int]) (map read $ drop 3 $ words s :: [Int])
+
+main = interact $ comp
