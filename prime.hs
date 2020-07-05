b4f88e2bf7993af664b72198e5b646262c09ad82
shadowfacsimile <shadow.facsimile@gmail.com>
2020-07-06 01:04:35 +0530

    Prime correction

--- prime.hs
+++ prime.hs
@@ -1,4 +1,7 @@
 isPrime :: Int -> String
 isPrime n | n < 2 = "Not Prime"
-          | null [x | x <- [2..ceiling(sqrt(fromIntegral n))], n `mod` x == 0] = "Prime"
-          | otherwise = "Not Prime"; 
+          | n == 2 = "Prime"
+          | null $ filter (== 0) $ map (mod n) [2..ceiling $ sqrt $ fromIntegral n] = "Prime"
+          | otherwise = "Not Prime"
+
+first100 = map (\x -> fst x) $ filter (\x -> snd x == "Prime") $ zip [1..100] (map isPrime [1..100])
