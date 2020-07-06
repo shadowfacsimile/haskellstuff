d225f52568d7fe7291e43bf3ce2e005f3e5927a1
shadowfacsimile <shadow.facsimile@gmail.com>
2020-07-06 21:49:26 +0530

    Refactoring

--- hckrnk_staircase.hs
+++ hckrnk_staircase.hs
@@ -1,11 +1,8 @@
 stair :: Int -> Int -> String
 stair m n | n == 0 = ""
-          | otherwise = (stair m (n - 1)) ++ 
-                          (unwords $ replicate (m - n + 1) "") ++ 
-                            (filter (/=' ') $ unwords $ replicate n "#") ++ 
-                               "\n"
+          | otherwise = (stair m (n - 1)) ++ (replicate (m - n) ' ') ++ (replicate n '#') ++ "\n"
 
 inp :: String -> String
 inp n = stair (read n :: Int) (read n :: Int)
 
-main = interact $ inp 
+main = interact $ inp
